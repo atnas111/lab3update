7bf29a38a575cdc2794eb53d628bf2ca5556759c
atnas111 <144899113+atnas111@users.noreply.github.com>
2023-12-16 08:38:41 +0300

    Delete Characters directory

--- Characters/AbstaractCharacter.java
+++
@@ -1,8 +0,0 @@
-package Characters;
-
-public abstract class AbstaractCharacter {
-    abstract void setName();
-    abstract void setWeight();
-    abstract void setHeight();
-    abstract void setGender();
-}
